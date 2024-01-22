34d4a42f4956500e65ffeb0fd8e695ecc7cd397c
carlaaguero <c.agueromiranda@gmail.com>
2024-01-22 19:24:28 +0100

    Adding Form submission using EmailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,10 +3,25 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a4lf0q5', 'template_83wki59', form.current, 'o4P7u8u4sJRhc8SgD')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -34,7 +49,7 @@ const Contact = () => {
 
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
            <input type="text" name='name' placeholder='Your Full Name' required />
            <input type="email" name='email' placeholder='Your Email' required />
            <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
